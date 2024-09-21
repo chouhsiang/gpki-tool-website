f0b5d0eac093885f222e21a2d9e56110b3db6a10
周詳 <champagne0906@gmail.com>
2024-09-21 21:06:38 +0800

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,3 +1,14 @@
 var script = document.createElement("script");
 script.src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js";
 document.body.appendChild(script);
+
+fetch("https://chouhsiang.github.io/gpki-tool-website/navbar.html") 
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.text();
+  })
+  .then((htmlContent) => {
+    document.body.innerHTML += htmlContent;
+  });
