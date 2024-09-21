973f7c3602f3348f34fd4151cfc99e6d0bbef6dc
周詳 <champagne0906@gmail.com>
2024-09-21 22:36:02 +0800

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,19 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var script = document.createElement("script");
-  script.src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js";
-  document.body.appendChild(script);
+var script = document.createElement("script");
+script.src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js";
+document.body.appendChild(script);
   
-  fetch("https://chouhsiang.github.io/gpki-tool-website/navbar.html") 
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.text();
-    })
-    .then((htmlContent) => {
-      document.body.innerHTML += htmlContent;
-    });
-}
+fetch("https://chouhsiang.github.io/gpki-tool-website/navbar.html") 
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.text();
+  })
+  .then((htmlContent) => {
+    document.body.innerHTML += htmlContent;
+  });
 
 function gocodepen() {
   var path = window.location.pathname;
