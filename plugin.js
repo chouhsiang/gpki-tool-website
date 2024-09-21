81e732beb101bcc2ae2d0bbbead1987998c6b617
周詳 <champagne0906@gmail.com>
2024-09-21 23:17:55 +0800

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -7,16 +7,4 @@ fetch("https://chouhsiang.github.io/gpki-tool-website/navbar.html")
     })
     .then((htmlContent) => {
       document.body.innerHTML += htmlContent;
-      function gocodepen() {
-        var path = window.location.pathname;
-        var id = path.substring(path.lastIndexOf('/') + 1); 
-        window.open('https://codepen.io/chouhsiang/pen/' + id, '_blank');
-      }
-      document.getElementById("gocodepen").addEventListener("click", gocodepen);
-      function gogithub() {
-        var path = window.location.pathname;
-        var id = path.substring(path.lastIndexOf('/') + 1); 
-        window.open('https://github.com/chouhsiang/codepen/blob/gh-pages/' + id + '.html', '_blank');  
-      }
-      document.getElementById("gogithub").addEventListener("click", gogithub);
     });
