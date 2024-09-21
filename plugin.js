29b99b2593dc0c6c7d4068477e44d1f3f67ca2a3
周詳 <champagne0906@gmail.com>
2024-09-21 22:54:35 +0800

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,26 +1,25 @@
-var script = document.createElement("script");
-script.src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js";
-document.body.appendChild(script);
-  
-fetch("https://chouhsiang.github.io/gpki-tool-website/navbar.html") 
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    return response.text();
-  })
-  .then((htmlContent) => {
-    document.body.innerHTML += htmlContent;
-  });
-
-function gocodepen() {
-  var path = window.location.pathname;
-  var id = path.substring(path.lastIndexOf('/') + 1); 
-  window.open('https://codepen.io/chouhsiang/pen/' + id, '_blank');
-}
-
-function gogithub() {
-  var path = window.location.pathname;
-  var id = path.substring(path.lastIndexOf('/') + 1); 
-  window.open('https://github.com/chouhsiang/codepen/blob/gh-pages/' + id + '.html', '_blank');  
+export function loadBootstrap() {
+  import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js';
+  fetch("https://chouhsiang.github.io/gpki-tool-website/navbar.html") 
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.text();
+    })
+    .then((htmlContent) => {
+      document.body.innerHTML += htmlContent;
+      function gocodepen() {
+        var path = window.location.pathname;
+        var id = path.substring(path.lastIndexOf('/') + 1); 
+        window.open('https://codepen.io/chouhsiang/pen/' + id, '_blank');
+      }
+      document.getElementById("gocodepen").addEventListener("click", gocodepen);
+      function gogithub() {
+        var path = window.location.pathname;
+        var id = path.substring(path.lastIndexOf('/') + 1); 
+        window.open('https://github.com/chouhsiang/codepen/blob/gh-pages/' + id + '.html', '_blank');  
+      }
+      document.getElementById("gogithub").addEventListener("click", gogithub);
+    });
 }
