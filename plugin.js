d67a8aa7cf0eb8d1cf7dcb32be778ebdd9c63e63
周詳 <champagne0906@gmail.com>
2024-09-21 23:42:46 +0800

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,10 +1,9 @@
-fetch("https://chouhsiang.github.io/gpki-tool-website/navbar.html") 
+document.addEventListener('DOMContentLoaded', () => {
+  fetch("https://chouhsiang.github.io/gpki-tool-website/navbar.html") 
     .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
       return response.text();
     })
     .then((htmlContent) => {
-      document.body.innerHTML += htmlContent;
+      document.body.insertAdjacentHTML('beforeend', htmlContent);
     });
+});
